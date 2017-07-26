085315a292221adee155b2e3e972962311287733
Charles Howard <frodare@gmail.com>
2017-07-25 22:51:17 -0400

    functional logger

--- src/main/java/com/frodare/logging/JSONFormatter.java
+++ src/main/java/com/frodare/logging/JSONFormatter.java
@@ -1,14 +1,208 @@
+/*
+ * Copyright (C) 2017 Charles Howard
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ *
+ * Some of this code was obtained from minimal-json (Ralf Sternberg):
+ * https://github.com/ralfstx/minimal-json/blob/master/com.eclipsesource.json/src/main/java/com/eclipsesource/json/JsonWriter.java
+ *
+ * JSON formatter was done manually in this class to avoid requiring dependencies.
+ */
+
 package com.frodare.logging;
 
-
 import java.util.logging.Formatter;
+import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 public class JSONFormatter extends Formatter {
 
+  private static final int CONTROL_CHARACTERS_END = 0x001f;
+
+  private static final char[] QUOT_CHARS = {'\\', '"'};
+  private static final char[] BS_CHARS = {'\\', '\\'};
+  private static final char[] LF_CHARS = {'\\', 'n'};
+  private static final char[] CR_CHARS = {'\\', 'r'};
+  private static final char[] TAB_CHARS = {'\\', 't'};
+
+  /*
+   * In JavaScript, U+2028 and U+2029 characters count as line endings and must be encoded.
+   * http://stackoverflow.com/questions/2965293/javascript-parse-error-on-u2028-unicode-character
+   */
+  private static final char[] UNICODE_2028_CHARS = {'\\', 'u', '2', '0', '2', '8'};
+  private static final char[] UNICODE_2029_CHARS = {'\\', 'u', '2', '0', '2', '9'};
+
+  private static final char[] HEX_DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+      'a', 'b', 'c', 'd', 'e', 'f'};
+
+  private static final String NEW_LINE = System.getProperty("line.separator");
+
   @Override
   public String format(LogRecord record) {
-    return record.getMessage() + System.getProperty("line.separator");
+    StringBuilder buf = new StringBuilder();
+    buf.append("{");
+    writeStringEntry(buf, "level", s(record.getLevel()));
+    buf.append(",");
+    writeStringEntry(buf, "message", record.getMessage());
+    buf.append(",");
+    writeLongEntry(buf, "millis", record.getMillis());
+    buf.append(",");
+    writeLongEntry(buf, "seqNum", record.getSequenceNumber());
+    buf.append(",");
+    writeSource(buf, "source", record);
+    buf.append(",");
+    writeThrown(buf, "thrown", record);
+    buf.append("}");
+    buf.append(NEW_LINE);
+    return buf.toString();
+  }
+
+  private void writeThrown(StringBuilder buf, String key, LogRecord record) {
+    writeQuotedString(buf, key);
+    buf.append(":");
+    if (record.getThrown() == null) {
+      buf.append("null");
+      return;
+    }
+    buf.append('"');
+    jsonEncodeString(buf, getStackTrace(record.getThrown()));
+    buf.append('"');
+  }
+
+  private String getStackTrace(Throwable t) {
+    if (t != null) {
+      StringBuilder buf = new StringBuilder();
+      writeStackTrace(buf, t);
+      return buf.toString();
+    }
+    return "null";
+  }
+
+  private void writeStackTrace(StringBuilder buf, Throwable t) {
+    if (t == null) {
+      return;
+    }
+    buf.append("Exception in ");
+    buf.append(t.getClass());
+    buf.append(": ");
+    buf.append(t.getMessage()).append(NEW_LINE);
+    for (StackTraceElement e : t.getStackTrace()) {
+      writeStackTraceElement(buf, e);
+    }
+    writeStackTrace(buf, t.getCause());
+  }
+
+  private static void writeStackTraceElement(StringBuilder buf, StackTraceElement e) {
+    buf.append("    at ");
+    buf.append(e.getClassName()).append(".").append(e.getMethodName());
+    buf.append("(").append(e.getFileName()).append(":").append(e.getLineNumber()).append(")");
+    buf.append(NEW_LINE);
+  }
+
+  private static String s(Level level) {
+    if (level == null) {
+      return "null";
+    }
+    return level.toString();
+  }
+
+  private static void writeSource(StringBuilder buf, String key, LogRecord record) {
+    String clazz = record.getSourceClassName();
+    String method = record.getSourceMethodName();
+
+    writeQuotedString(buf, key);
+    buf.append(":");
+
+    if (clazz == null) {
+      buf.append("null");
+      return;
+    }
+
+    buf.append('"');
+    jsonEncodeString(buf, record.getSourceClassName());
+
+    if (method == null) {
+      buf.append('"');
+      return;
+    }
+
+    buf.append(".");
+    jsonEncodeString(buf, record.getSourceMethodName());
+    buf.append("()");
+    buf.append('"');
+  }
+
+  private static void writeStringEntry(StringBuilder buf, String key, String value) {
+    writeQuotedString(buf, key);
+    buf.append(":");
+    writeQuotedString(buf, value);
+  }
+
+  private static void writeLongEntry(StringBuilder buf, String key, long value) {
+    writeQuotedString(buf, key);
+    buf.append(":");
+    buf.append(value);
+  }
+
+  private static void writeQuotedString(StringBuilder buf, String s) {
+    if (s == null) {
+      jsonEncodeString(buf, s);
+      return;
+    }
+    buf.append('"');
+    jsonEncodeString(buf, s);
+    buf.append('"');
+  }
+
+  private static void jsonEncodeString(StringBuilder buf, String s) {
+    if (s == null) {
+      buf.append("null");
+      return;
+    }
+    char[] chars = s.toCharArray();
+    char[] replacement;
+    for (char c : chars) {
+      replacement = getReplacementChars(c);
+      if (replacement == null) {
+        buf.append(c);
+      } else {
+        buf.append(replacement);
+      }
+    }
+  }
+
+  private static char[] getReplacementChars(char ch) {
+    if (ch > '\\') {
+      if (ch < '\u2028' || ch > '\u2029') {
+        // The lower range contains 'a' .. 'z'. Only 2 checks required.
+        return null;
+      }
+      return ch == '\u2028' ? UNICODE_2028_CHARS : UNICODE_2029_CHARS;
+    }
+    if (ch == '\\') {
+      return BS_CHARS;
+    }
+    if (ch > '"') {
+      // This range contains '0' .. '9' and 'A' .. 'Z'. Need 3 checks to get here.
+      return null;
+    }
+    if (ch == '"') {
+      return QUOT_CHARS;
+    }
+    if (ch > CONTROL_CHARACTERS_END) {
+      return null;
+    }
+    if (ch == '\n') {
+      return LF_CHARS;
+    }
+    if (ch == '\r') {
+      return CR_CHARS;
+    }
+    if (ch == '\t') {
+      return TAB_CHARS;
+    }
+    return new char[]{'\\', 'u', '0', '0', HEX_DIGITS[ch >> 4 & 0x000f], HEX_DIGITS[ch & 0x000f]};
   }
 
 }
--- src/test/java/com/frodare/logging/JSONFormatterTest.java
+++ src/test/java/com/frodare/logging/JSONFormatterTest.java
@@ -1,11 +1,12 @@
 package com.frodare.logging;
 
-import java.util.logging.ConsoleHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
+import mockit.Deencapsulation;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class JSONFormatterTest {
@@ -13,7 +14,7 @@ public class JSONFormatterTest {
   private static final Logger LOG = Logger.getLogger(JSONFormatter.class.getName());
 
   static {
-    for(Handler handler : Logger.getLogger("").getHandlers()){
+    for (Handler handler : Logger.getLogger("").getHandlers()) {
       handler.setFormatter(new JSONFormatter());
     }
   }
@@ -21,14 +22,133 @@ public class JSONFormatterTest {
   private final JSONFormatter formatter = new JSONFormatter();
 
   @Test
-  public void format() throws Exception {
-    LogRecord r = new LogRecord(Level.INFO, "Test");
-   // Assert.assertEquals("Test", formatter.format(r));
-    LOG.log(Level.INFO, "HOWDY!");
+  public void format() {
+    LogRecord r = new LogRecord(Level.INFO, "howdy!");
+    r.setSourceMethodName("meth");
+    r.setSourceClassName("com.frodare.Test");
+    r.setMillis(1001);
+    r.setSequenceNumber(15);
+    String actual = Deencapsulation.invoke(formatter, "format", r);
+    String expected = "{\"level\":\"INFO\",\"message\":\"howdy!\",\"millis\":1001,\"seqNum\":15,\"source\":\"com.frodare.Test.meth()\",\"thrown\":null}\n";
+    Assert.assertEquals(expected, actual);
+  }
 
-    LOG.log(Level.INFO, "HOWDY!");
+  @Test
+  public void writeStackTraceElement() {
+    StackTraceElement e = new StackTraceElement("com.frodare.Foo", "meth", "Test.class", 420);
+    writeStackTraceElement(e, "    at com.frodare.Foo.meth(Test.class:420)\n");
+  }
 
-    LOG.log(Level.INFO, "HOWDY!");
+  private void writeStackTraceElement(StackTraceElement in, String out) {
+    StringBuilder s = new StringBuilder();
+    Deencapsulation.invoke(JSONFormatter.class, "writeStackTraceElement", new Class<?>[]{StringBuilder.class, StackTraceElement.class}, s, in);
+    Assert.assertEquals(out, s.toString());
+  }
+
+  @Test
+  public void writeSource() {
+    LogRecord r = new LogRecord(Level.INFO, "foo");
+    r.setSourceClassName("Test.class");
+    r.setSourceMethodName("invokeMe");
+    writeSource(r, "\"key\":\"Test.class.invokeMe()\"");
+  }
+
+  @Test
+  public void writeSourceNulls() {
+    LogRecord r = new LogRecord(Level.INFO, "foo");
+    writeSource(r, "\"key\":null");
+  }
+
+  @Test
+  public void writeSourceNullMethod() {
+    LogRecord r = new LogRecord(Level.INFO, "foo");
+    r.setSourceClassName("Test.class");
+    writeSource(r, "\"key\":\"Test.class\"");
+  }
+
+  private void writeSource(LogRecord in, String out) {
+    StringBuilder s = new StringBuilder();
+    Deencapsulation.invoke(JSONFormatter.class, "writeSource", new Class<?>[]{StringBuilder.class, String.class, LogRecord.class}, s, "key", in);
+    Assert.assertEquals(out, s.toString());
+  }
+
+  @Test
+  public void writeStringEntry() {
+    writeStringEntry("test", "\"key\":\"test\"");
+    writeStringEntry("foo\tbar", "\"key\":\"foo\\tbar\"");
+    writeStringEntry(null, "\"key\":null");
+  }
+
+  private void writeStringEntry(String in, String out) {
+    StringBuilder s = new StringBuilder();
+    Deencapsulation.invoke(JSONFormatter.class, "writeStringEntry", new Class<?>[]{StringBuilder.class, String.class, String.class}, s, "key", in);
+    Assert.assertEquals(out, s.toString());
+  }
+
+  @Test
+  public void writeLongEntry() {
+    writeLongEntry(1, "\"key\":1");
+    writeLongEntry(0, "\"key\":0");
+  }
+
+  private void writeLongEntry(long in, String out) {
+    StringBuilder s = new StringBuilder();
+    Deencapsulation.invoke(JSONFormatter.class, "writeLongEntry", s, "key", in);
+    Assert.assertEquals(out, s.toString());
+  }
+
+  @Test
+  public void writeQuotedString() {
+    writeQuotedString("test\n", "\"test\\n\"");
+    writeQuotedString("foo", "\"foo\"");
+    writeQuotedString("", "\"\"");
+    writeQuotedString("null", "\"null\"");
+    writeQuotedString(null, "null");
+  }
+
+  private void writeQuotedString(String in, String out) {
+    StringBuilder s = new StringBuilder();
+    Deencapsulation.invoke(JSONFormatter.class, "writeQuotedString", new Class<?>[]{StringBuilder.class, String.class}, s, in);
+    Assert.assertEquals(out, s.toString());
+  }
+
+  @Test
+  public void jsonEncodeString() {
+    jsonEncodeString("test\n", "test\\n");
+    jsonEncodeString("foo", "foo");
+    jsonEncodeString("", "");
+    jsonEncodeString(null, "null");
+  }
+
+  private void jsonEncodeString(String in, String out) {
+    StringBuilder s = new StringBuilder();
+    Deencapsulation.invoke(JSONFormatter.class, "jsonEncodeString", new Class<?>[]{StringBuilder.class, String.class}, s, in);
+    Assert.assertEquals(out, s.toString());
+  }
+
+  @Test
+  public void getReplacementChars() {
+    getReplacementChars('\n', "\\n");
+    getReplacementChars('\t', "\\t");
+    getReplacementChars('s', null);
+  }
+
+  private void getReplacementChars(Character in, String out) {
+    char[] ca = Deencapsulation.invoke(JSONFormatter.class, "getReplacementChars", new Class<?>[]{Character.class}, in);
+    Assert.assertEquals(out, s(ca));
+  }
+
+  private String s(char[] c) {
+    if (c == null) {
+      return null;
+    }
+    return new String(c);
+  }
+
+  @Ignore
+  @Test
+  public void testActualLog() throws Exception {
+    LOG.log(Level.INFO, "TEST MESSAGE", new RuntimeException("Level 1", new IllegalArgumentException("level 2")));
   }
 
 }
